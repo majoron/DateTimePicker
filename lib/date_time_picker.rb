3a904e20899e4005e980b6b741f3f017d3dd7e4c
Alexey Tsokurov <me@ximik.net>
2012-03-08 16:18:10 +0400

    RDoc + gitmodule

--- lib/date_time_picker.rb
+++ lib/date_time_picker.rb
@@ -1,6 +1,9 @@
-require 'date_time_picker/date_time_picker_helper'
+# :main: README.rdoc
 
-module DateTimePicker
-
+module DateTimePicker # :nodoc:
+  class Engine < ::Rails::Engine # :nodoc:
+  end
 end
 
+dir = File.expand_path(File.dirname(__FILE__))
+require File.join(dir, 'date_time_picker', 'date_time_picker_helpers')
