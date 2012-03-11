3dc61803f2c64f9f7b2af451b1aea65a0b07db9e
Artem Rufanov <arufanov@gmail.com>
2012-03-11 23:20:56 +0400

    Auxiliary files

---
+++ distrib/_local_install.cmd
@@ -0,0 +1,19 @@
+echo off
+
+Rem: set path 
+SET PATH=
+SET PATH=%PATH%;D:/Binutils/Ruby192/bin
+SET PATH=%PATH%;C:/Binutils/Ruby192/bin
+SET PATH=%PATH%;D:/Binutils/Ruby187/bin
+SET PATH=%PATH%;C:/Binutils/Ruby187/bin
+SET PATH=%PATH%;D:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;C:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;D:/Binutils/rubygems-1.3.5/bin
+SET PATH=%PATH%;C:/Binutils/rubygems-1.3.5/bin
+
+echo on
+Rem: set enviroment and execute ant script
+gem install ./output/date_time_picker-0.5.0.gem --local
+rem gem uninstall date_time_picker
+rem gem list --local
+
---
+++ distrib/_local_release.cmd
@@ -0,0 +1,19 @@
+echo off
+rem examples of usage
+rem
+
+
+Rem: set path 
+SET PATH=
+SET PATH=%PATH%;D:/Binutils/Ruby192/bin
+SET PATH=%PATH%;C:/Binutils/Ruby192/bin
+SET PATH=%PATH%;D:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;C:/Binutils/Ruby-186-27/bin
+SET PATH=%PATH%;D:/Binutils/rubygems-1.3.5/bin
+SET PATH=%PATH%;C:/Binutils/rubygems-1.3.5/bin
+
+Rem: set enviroment and release gem
+cd ./..
+gem release
+cd ./distrib
+
