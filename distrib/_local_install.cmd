b787f564bb9603c069b197c1b085762c536bd982
Artem Rufanov <arufanov@gmail.com>
2012-03-21 00:48:47 +0400

    0.5.2

--- distrib/_local_install.cmd
+++ distrib/_local_install.cmd
@@ -13,7 +13,7 @@ SET PATH=%PATH%;C:/Binutils/rubygems-1.3.5/bin
 
 echo on
 Rem: set enviroment and execute ant script
-gem install ./output/date_time_picker-0.5.1.gem --local
+gem install ./output/date_time_picker-0.5.2.gem --local
 rem gem uninstall date_time_picker
 rem gem list --local
 
